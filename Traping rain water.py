614085abee3cf1760558280e4725390e6e5ebfa3
Basheerun Shaik <sbasheerun009@gmail.com>
2026-06-20 10:42:15 +0530

    updating code

--- Traping rain water.py
+++ Traping rain water.py
@@ -2,23 +2,15 @@ N = int(input())
 height = list(map(int, input().split()))
 
 left = 0
-right = N - 1
-left_max = 0
-right_max = 0
-water = 0
+right = N-1
+water_max = 0
 
 while left < right:
-    if height[left] < height[right]:
-        if height[left] >= left_max:
-            left_max = height[left]
-        else:
-            water += left_max - height[left]
+    length = min(height[left],height[right])
+    width = right - left
+    water_max = max(water_max,length*width)
+    if height[right] < height[left]:
         left += 1
     else:
-        if height[right] >= right_max:
-            right_max = height[right]
-        else:
-            water += right_max - height[right]
         right -= 1
-
-print(water)
+print(water_max)
